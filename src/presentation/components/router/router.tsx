f1dd01669f0d0ee350ff3ac205326cf78cdc817f
Kaua Hl <profissional.kaua@gmail.com>
2022-02-28 12:59:26 -0300

    feat: ensure login present error if email is invalid

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "@/presentation/pages";
 
+import { ValidationSpy } from "@/presentation/test"; //Remover
+
 const Router: React.FC = () => {
+  const validationSpy = new ValidationSpy(); //Remover
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login validation={validationSpy} />} />
       </Routes>
     </BrowserRouter>
   );
