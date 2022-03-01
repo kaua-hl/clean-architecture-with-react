262fd7085ab753b649c5c7670bb9086e45bc5124
Kaua Hl <profissional.kaua@gmail.com>
2022-03-01 12:06:43 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/requiredField/requireFieldValidation.spec.ts
+++ src/validation/requiredField/requireFieldValidation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldError } from "../errors/";
 import { RequiredFieldValidation } from "./requiredFieldValidation";
+import faker from "@faker-js/faker";
 
 describe("RequireFieldValidation", () => {
   test("Should return error if field is empty", () => {
     const sut = new RequiredFieldValidation("email");
     const error = sut.validate("");
     expect(error).toEqual(new RequiredFieldError());
+  });
+
+  test("Should return falsy if field is not empty", () => {
+    const sut = new RequiredFieldValidation("email");
+    const error = sut.validate(faker.random.word());
+    expect(error).toBeFalsy();
   });
 });
